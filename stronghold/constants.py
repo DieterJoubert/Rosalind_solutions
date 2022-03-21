1daa1d16c8e1b2769789d0711dd5bef9a4a54f64
Dieter Joubert <dieterjoubert@gmail.com>
2022-03-20 21:49:36 -0700

    update COMPLEMENT_MAP to DNA_COMPLETEMENT_MAP

--- stronghold/constants.py
+++ stronghold/constants.py
@@ -1,6 +1,6 @@
 VALID_DNA_SYMBOLS = {'A', 'C', 'G', 'T'}
 
-COMPLEMENT_MAP = {
+DNA_COMPLEMENT_MAP = {
     'A': 'T',
     'T': 'A',
     'C': 'G',
--- stronghold/prob_revc.py
+++ stronghold/prob_revc.py
@@ -1,11 +1,11 @@
 from utils import get_dna_data, write_solution
-from constants import COMPLEMENT_MAP
+from constants import DNA_COMPLEMENT_MAP
 
 INPUT_PATH = './input/rosalind_revc.txt'
 OUTPUT_PATH = './output/prob_revc.txt'
 
 def get_reverse_complement(dna: str) -> str:
-    return "".join([COMPLEMENT_MAP[c] for c in dna[::-1]])
+    return "".join([DNA_COMPLEMENT_MAP[c] for c in dna[::-1]])
 
 def main():
     data = get_dna_data(INPUT_PATH)
