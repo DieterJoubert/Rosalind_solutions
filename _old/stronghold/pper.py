a5c115b05e4463536763ed4c6d18b1acd06840cb
Dieter Joubert <dieterjoubert@gmail.com>
2022-03-20 21:30:08 -0700

    delete _old

--- _old/stronghold/pper.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def pper(n,k):
-  print ( math.factorial(n) / math.factorial(n-k)  ) % 1000000
--- _old/village/ini4.py
+++
@@ -1,7 +0,0 @@
-def sumOdd(x,y):
-  sum = 0
-  while x <= y:
-    if x % 2 == 1:
-      sum += x
-    x += 1
-  return sum
